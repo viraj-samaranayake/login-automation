53cf0ed1dc17d0823281f8716f6fc6aef2731f0b
codevrj <isuruviraj25@gmail.com>
2026-01-01 13:18:12 +0530

    DataProviders class updated

--- src/test/java/utilities/DataProviders.java
+++ src/test/java/utilities/DataProviders.java
@@ -2,6 +2,8 @@ package utilities;
 
 import org.testng.annotations.DataProvider;
 
+import java.io.IOException;
+
 public class DataProviders {
 
     // DataProvider 1 - Hardcoded data
@@ -15,4 +17,29 @@ public class DataProviders {
                 {"userInvalid", "invalid","Invalid"}
         };
     }
+
+
+    // DataProvider 2 - Get data from excel file
+
+    String excelFilePath = "./testData/TestData.xlsx";
+    String xlSheet = "LoginCredentials";
+    ExcelUtility dt = new ExcelUtility(excelFilePath);
+
+    // This method get data from the Excel file via the ExcelUtils class...
+    @DataProvider(name = "LoginData")
+    public String[][] getData() throws IOException {
+
+        int totRowNum = dt.getRowCount(xlSheet);
+        int totColNum = dt.getCellCount(xlSheet, 1);
+
+        String [][] regData = new String[totRowNum][totColNum];
+
+        for (int i = 1; i <= totRowNum; i++) {
+            for (int j = 0; j < totColNum; j++) {
+                regData[i - 1][j] = dt.getCellData(xlSheet, i, j); // 1 row  0 column
+            }
+        }
+        return regData;
+    }
+
 }
